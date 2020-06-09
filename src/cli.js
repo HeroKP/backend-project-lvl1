4d1bb9cf6205d7fcc754cb76ceac41094f5851bc
Kirill1 <KirillTheDragon1@MacBook-Air-2.local>
2020-06-09 18:06:15 +0200

    Testing

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-  console.log(`Hi ${readlineSync.question('May I have your name? ')}!`);
+console.log(`Hi ${readlineSync.question('May I have your name? ')}!`);
 };
