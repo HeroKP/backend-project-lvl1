54d5eeec73e69e5370e7e6a9b17df07088f394f1
Kirill1 <KirillTheDragon1@macbook-air-2.home>
2020-07-10 15:12:26 +0200

    Polishing

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	npm install
 
 brain-games:
-	node src/bin/brain-games.js
+	node src/bin/menu.js
 
 publish:
 	npm publish --dry-run
--- src/brain-games.js
+++ src/brain-games.js
@@ -1,13 +1,10 @@
-#!/usr/bin/env node
-/* eslint-disable no-unused-vars */
-
 import readlineSync from 'readline-sync';
-import playGame from './playGame.js';
-import calculator from '../games/calculator.js';
-import even from '../games/even.js';
-import gcd from '../games/gcd.js';
-import prime from '../games/prime.js';
-import progression from '../games/progression.js';
+import playGame from './bin/playGame.js';
+import calculator from './games/calculator.js';
+import even from './games/even.js';
+import gcd from './games/gcd.js';
+import prime from './games/prime.js';
+import progression from './games/progression.js';
 
 const username = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ') || 'Anonymous';
 
@@ -20,7 +17,6 @@ const games = {
 };
 
 const gameNames = Object.keys(games);
-const affirmativeResponses = ['yes', 'Yes', 'yep', 'Yep', 'sure', 'Sure', 'Go go power rangers!'];
 
 const playGames = () => {
   const gameNumber = readlineSync.keyInSelect(gameNames, 'Which game would you like to play?');
@@ -31,3 +27,5 @@ const playGames = () => {
 };
 
 playGames();
+
+export default () => playGames;
